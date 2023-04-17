6ad700c4fe9fc368a3ab3259aac21b753963226b
dlselrpa <dlselrpa@naver.com>
2023-04-17 10:55:22 +0900

    rtx dlss modifier ans scroll ui

---
+++ Plugins/DLSS/Intermediate/Build/Win64/UE4/Inc/DLSSBlueprint/DLSSLibrary.generated.h
@@ -0,0 +1,156 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+enum class UDLSSMode : uint8;
+struct FVector2D;
+enum class UDLSSSupport : uint8;
+#ifdef DLSSBLUEPRINT_DLSSLibrary_generated_h
+#error "DLSSLibrary.generated.h already included, missing '#pragma once' in DLSSLibrary.h"
+#endif
+#define DLSSBLUEPRINT_DLSSLibrary_generated_h
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_SPARSE_DATA
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execGetDefaultDLSSMode); \
+	DECLARE_FUNCTION(execGetDLSSSharpness); \
+	DECLARE_FUNCTION(execSetDLSSSharpness); \
+	DECLARE_FUNCTION(execGetDLSSMode); \
+	DECLARE_FUNCTION(execSetDLSSMode); \
+	DECLARE_FUNCTION(execGetDLSSScreenPercentageRange); \
+	DECLARE_FUNCTION(execGetDLSSModeInformation); \
+	DECLARE_FUNCTION(execGetSupportedDLSSModes); \
+	DECLARE_FUNCTION(execIsDLSSModeSupported); \
+	DECLARE_FUNCTION(execIsDLAAEnabled); \
+	DECLARE_FUNCTION(execEnableDLAA); \
+	DECLARE_FUNCTION(execGetDLSSMinimumDriverVersion); \
+	DECLARE_FUNCTION(execQueryDLSSSupport); \
+	DECLARE_FUNCTION(execIsDLSSSupported);
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execGetDefaultDLSSMode); \
+	DECLARE_FUNCTION(execGetDLSSSharpness); \
+	DECLARE_FUNCTION(execSetDLSSSharpness); \
+	DECLARE_FUNCTION(execGetDLSSMode); \
+	DECLARE_FUNCTION(execSetDLSSMode); \
+	DECLARE_FUNCTION(execGetDLSSScreenPercentageRange); \
+	DECLARE_FUNCTION(execGetDLSSModeInformation); \
+	DECLARE_FUNCTION(execGetSupportedDLSSModes); \
+	DECLARE_FUNCTION(execIsDLSSModeSupported); \
+	DECLARE_FUNCTION(execIsDLAAEnabled); \
+	DECLARE_FUNCTION(execEnableDLAA); \
+	DECLARE_FUNCTION(execGetDLSSMinimumDriverVersion); \
+	DECLARE_FUNCTION(execQueryDLSSSupport); \
+	DECLARE_FUNCTION(execIsDLSSSupported);
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUDLSSLibrary(); \
+	friend struct Z_Construct_UClass_UDLSSLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UDLSSLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/DLSSBlueprint"), DLSSBLUEPRINT_API) \
+	DECLARE_SERIALIZER(UDLSSLibrary)
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_INCLASS \
+private: \
+	static void StaticRegisterNativesUDLSSLibrary(); \
+	friend struct Z_Construct_UClass_UDLSSLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UDLSSLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/DLSSBlueprint"), DLSSBLUEPRINT_API) \
+	DECLARE_SERIALIZER(UDLSSLibrary)
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	DLSSBLUEPRINT_API UDLSSLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UDLSSLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(DLSSBLUEPRINT_API, UDLSSLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UDLSSLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	DLSSBLUEPRINT_API UDLSSLibrary(UDLSSLibrary&&); \
+	DLSSBLUEPRINT_API UDLSSLibrary(const UDLSSLibrary&); \
+public:
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	DLSSBLUEPRINT_API UDLSSLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	DLSSBLUEPRINT_API UDLSSLibrary(UDLSSLibrary&&); \
+	DLSSBLUEPRINT_API UDLSSLibrary(const UDLSSLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(DLSSBLUEPRINT_API, UDLSSLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UDLSSLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UDLSSLibrary)
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_PRIVATE_PROPERTY_OFFSET
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_64_PROLOG
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_PRIVATE_PROPERTY_OFFSET \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_SPARSE_DATA \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_RPC_WRAPPERS \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_INCLASS \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_PRIVATE_PROPERTY_OFFSET \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_SPARSE_DATA \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_RPC_WRAPPERS_NO_PURE_DECLS \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_INCLASS_NO_PURE_DECLS \
+	Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h_68_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DLSSBLUEPRINT_API UClass* StaticClass<class UDLSSLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Game_Planning_Task_Plugins_DLSS_Source_DLSSBlueprint_Public_DLSSLibrary_h
+
+
+#define FOREACH_ENUM_UDLSSMODE(op) \
+	op(UDLSSMode::Off) \
+	op(UDLSSMode::Auto) \
+	op(UDLSSMode::UltraQuality) \
+	op(UDLSSMode::Quality) \
+	op(UDLSSMode::Balanced) \
+	op(UDLSSMode::Performance) \
+	op(UDLSSMode::UltraPerformance) 
+
+enum class UDLSSMode : uint8;
+template<> DLSSBLUEPRINT_API UEnum* StaticEnum<UDLSSMode>();
+
+#define FOREACH_ENUM_UDLSSSUPPORT(op) \
+	op(UDLSSSupport::Supported) \
+	op(UDLSSSupport::NotSupported) \
+	op(UDLSSSupport::NotSupportedIncompatibleHardware) \
+	op(UDLSSSupport::NotSupportedDriverOutOfDate) \
+	op(UDLSSSupport::NotSupportedOperatingSystemOutOfDate) \
+	op(UDLSSSupport::NotSupportedByPlatformAtBuildTime) \
+	op(UDLSSSupport::NotSupportedIncompatibleAPICaptureToolActive) 
+
+enum class UDLSSSupport : uint8;
+template<> DLSSBLUEPRINT_API UEnum* StaticEnum<UDLSSSupport>();
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
