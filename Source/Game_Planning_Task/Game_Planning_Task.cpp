424165b08b4a885d258a2074eabbca46158be38c
dlselrpa <dlselrpa@naver.com>
2023-04-23 20:39:59 +0900

    chage to blue print project

--- Source/Game_Planning_Task/Game_Planning_Task.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Game_Planning_Task.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Planning_Task, "Game_Planning_Task" );
- 
