16a232f445cd9f31b7a2b233b0e5b2b07256ded8
dlselrpa <103366468+iciency@users.noreply.github.com>
2023-04-25 22:31:39 +0900

    add c++ project

---
+++ Source/Game_Planning_Task/Game_Planning_Task.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game_Planning_Task.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Planning_Task, "Game_Planning_Task" );
